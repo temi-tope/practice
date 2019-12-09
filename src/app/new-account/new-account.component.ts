c03a1fedc552059b0480e6b071a3d8a64dcc9d22
temi-tope <temitope.ogungbesan.5@gmail.com>
2019-12-09 13:02:47 +0100

    contd

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers:  [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
+  constructor(private loggingService: LoggingService,
+              private accountsService: AccountsService) {
+  }
+
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    // tslint:disable-next-line: no-unused-expression
+    this.accountsService.addAcount(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
